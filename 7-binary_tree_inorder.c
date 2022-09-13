9a620a8dacdf2116c7ae46c7f7244cf687aeda19
alejandrodgz <5199@holbertonschool.com>
2022-09-12 23:15:12 -0500

    task 7 v1

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * traverse - traverse tree
+ * @trav: tree to traverse
+ * @func: function
+ */
+
+
+void traverse(binary_tree_t *trav, void (*func)(int))
+{
+	if (trav == NULL)
+		return;
+	traverse(trav->left, func);
+	func(trav->n);
+	traverse(trav->right, func);
+}
+/**
+ * binary_tree_preorder - traverse a tree
+ * @tree: tree to traverse
+ * @func: function for printing
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	traverse(tree->left, func);
+	func(tree->n);
+	traverse(tree->right, func);
+}
