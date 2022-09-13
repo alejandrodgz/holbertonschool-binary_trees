56475929fa01041328e7bc9613ceedf94807a421
alejandrodgz <5199@holbertonschool.com>
2022-09-13 18:08:15 -0500

    task 10 v1

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - depth of tree
+ * @tree: tree to measure
+ * Return: depth
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	binary_tree_t *tmp = (binary_tree_t *)tree;
+	size_t a = 0;
+
+	if (tree->parent == NULL || tree == NULL)
+		return (0);
+	while (tmp != NULL)
+	{
+		tmp = tmp->parent;
+		a++;
+	}
+	a--;
+	return (a);
+}
