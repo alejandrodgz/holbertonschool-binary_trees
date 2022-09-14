c61f6e6db63cbfa28ba0e1379511e402767181b1
alejandrodgz <5199@holbertonschool.com>
2022-09-14 16:26:47 -0500

    task 17 v1

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - find sibiling of node
+ * @node: node to find it sibilings
+ * Return: sibilig
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *tmp = node, *tmp1 = node->parent;
+
+	if (node == NULL)
+		return (NULL);
+	if (node->parent == NULL)
+		return (NULL);
+	if (tmp1->left == NULL || tmp1->right == NULL)
+		return (NULL);
+	if (tmp1->right != tmp)
+		return (tmp1->right);
+	return (tmp1->left);
+}
