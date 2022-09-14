0a6cce7928774189674b6039fca4181c8af12644
alejandrodgz <5199@holbertonschool.com>
2022-09-14 16:54:53 -0500

    task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - find sibiling of node
+ * @node: node to find it sibilings
+ * Return: sibiling
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || node->parent == NULL)
+		return (NULL);
+	if (node->parent->parent == NULL)
+		return (NULL)
+	if (node->parent->parent->left == NULL || node->parent->parent->right == NULL)
+		return (NULL);
+	if (node->parent->parent->right != node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
+}
